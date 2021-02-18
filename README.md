a5df8025fd3ab182ae2511d525f708cbbe0ed43a
Mathieu Ablasou <alexherbo2@gmail.com>
2021-02-18 15:38:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# fifo.cr
+
+fifo.cr is a [Crystal] library for using [FIFOs][FIFO].
+
+[Crystal]: https://crystal-lang.org
+[FIFO]: https://en.wikipedia.org/wiki/FIFO_(computing_and_electronics)
+
+## Dependencies
+
+- [Crystal]
+
+## Installation
+
+1. Add the dependency to your `shard.yml`:
+
+``` yaml
+dependencies:
+  fifo:
+    github: alexherbo2/fifo.cr
+```
+
+2. Run `shards install`.
+
+## Usage
+
+``` crystal
+require "fifo"
+```
+
+See [`src/fifo.cr`] for a complete reference.
+
+[`src/fifo.cr`]: src/fifo.cr
---
+++ src/fifo.cr
@@ -0,0 +1,47 @@
+class FIFO
+  property path
+
+  def reader
+    File.new(path, "r")
+  end
+
+  def writer
+    File.new(path, "w")
+  end
+
+  def initialize(@path : Path)
+    create unless exists?
+  end
+
+  def self.new(path : String)
+    new(Path[path])
+  end
+
+  def self.new
+    new(Path[File.tempname])
+  end
+
+  def create
+    Process.run("mkfifo", { path.to_s })
+  end
+
+  def delete
+    File.delete(path)
+  end
+
+  def exists?
+    File.exists?(path)
+  end
+
+  def consume(&block)
+    value = yield self
+    delete
+    value
+  end
+
+  def self.consume(&block)
+    new.consume do |fifo|
+      yield fifo
+    end
+  end
+end
